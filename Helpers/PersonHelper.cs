c6864b41f968d1af975cde017951c2f25dfceffa
gabskatze <gabskatze@gmail.com>
2019-06-04 16:48:30 -0400

    Add files via upload

---
+++ Helpers/PersonHelper.cs
@@ -0,0 +1,20 @@
+﻿using CodeChallenge.Models;
+using System.Collections.Generic;
+
+namespace CodeChallenge.Helpers
+{
+    public class PersonHelper
+    {
+        public static List<Person> PersonList = new List<Person>();
+
+        public static void AddPerson(Person person)
+        {
+            PersonList.Add(person);
+        }
+        
+        public static List<Person> GetPeople()
+        {
+            return PersonList;
+        }
+    }
+}
