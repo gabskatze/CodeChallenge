3103bb325eb9bb59b8d003ef743c434457758b83
gabskatze <gabskatze@gmail.com>
2019-06-04 16:44:40 -0400

    Add files via upload

---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeChallenge.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+    }
+}
